564c4302af6ff7218690f1ddae5af88ade0bd064
oshada12 <107744144+oshada12@users.noreply.github.com>
2022-06-18 17:46:59 +0100

    gfhjfghfg

---
+++ language/help.js
@@ -0,0 +1,716 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/94783623904
+   * Follow https://github.com/zero7v
+*/
+
+// Menu
+exports.menu = (botname, pushname) => {
+	return`Haii Kak ${pushname} 🐦
+
+Saya ${botname}, Bot Ini Adalah Beta Multi-Device WhatsApp.
+Jika Kamu Menemukan Semacam Bug Atau Kesalahan Mohon Dimaklumi Dulu Ya, Lapor Owner Agar Segera Di Perbaiki🙏`
+}
+
+// List
+exports.list = (pushname) => {
+	return`Hai kak ${pushname}👋, 𝘞𝘌𝘓𝘓𝘊𝘖𝘔𝘌 𝘛𝘖 𝘔𝘌𝘕𝘜 𝖡𝗈𝗍 𝖢𝗋𝖾𝖺𝗍𝖾 𝖡𝗒 𝖵𝗂𝗆𝗎𝗄𝗍𝗁𝗂 𝖮𝗌𝗁𝖺𝖽𝖺🐦`
+}
+
+// All Menu
+exports.allmenu = (prefix) => {
+return`
+┌── 「 *Group Menu* 」
+├ 👻💫✇ ${prefix}linkgroup
+├ 👻💫✇ ${prefix}ephemeral [option]
+├ 👻💫✇ ${prefix}setppgc [image]
+├ 👻💫✇ ${prefix}setname [text]
+├ 👻💫✇ ${prefix}setdesc [text]
+├ 👻💫✇ ${prefix}group [option]
+├ 👻💫✇ ${prefix}editinfo [option]
+├ 👻💫✇ ${prefix}add @user
+├ 👻💫✇ ${prefix}kick @user
+├ 👻💫✇ ${prefix}hidetag [text]
+├ 👻💫✇ ${prefix}tagall [text]
+├ 👻💫✇ ${prefix}antilink [on/off]
+├ 👻💫✇ ${prefix}mute [on/off]
+├ 👻💫✇ ${prefix}promote @user
+├ 👻💫✇ ${prefix}demote @user
+├ 👻💫✇ ${prefix}vote [text]
+├ 👻💫✇ ${prefix}devote
+├ 👻💫✇ ${prefix}upvote
+├ 👻💫✇ ${prefix}cekvote
+├ 👻💫✇ ${prefix}hapusvote
+└───────
+
+
+
+┌── 「 *Downloader Menu* 」
+├ 👻💫✇ ${prefix}tiktoknowm [url]
+├ 👻💫✇ ${prefix}tiktokwm [url]
+├ 👻💫✇ ${prefix}tiktokmp3 [url]
+├ 👻💫✇ ${prefix}instagram [url]
+├ 👻💫✇ ${prefix}twitter [url]
+├ 👻💫✇ ${prefix}twittermp3 [url]
+├ 👻💫✇ ${prefix}facebook [url]
+├ 👻💫✇ ${prefix}pinterestdl [url]
+├ 👻💫✇ ${prefix}ytmp3 [url]
+├ 👻💫✇ ${prefix}ytmp4 [url]
+├ 👻💫✇ ${prefix}getmusic [query]
+├ 👻💫✇ ${prefix}getvideo [query]
+├ 👻💫✇ ${prefix}umma [url]
+├ 👻💫✇ ${prefix}joox [query]
+├ 👻💫✇ ${prefix}soundcloud [url]
+└───────
+
+┌── 「 *Search Menu* 」
+├ 👻💫✇ ${prefix}play [query]
+├ 👻💫✇ ${prefix}yts [query]
+├ 👻💫✇ ${prefix}google [query]
+├ 👻💫✇ ${prefix}gimage [query]
+├ 👻💫✇ ${prefix}pinterest [query]
+├ 👻💫✇ ${prefix}wallpaper [query]
+├ 👻💫✇ ${prefix}wikimedia [query]
+├ 👻💫✇ ${prefix}ytsearch [query]
+├ 👻💫✇ ${prefix}ringtone [query]
+├ 👻💫✇ ${prefix}stalk [option] [query]
+├ 👻💫✇ ${prefix}webtoons [query]
+├ 👻💫✇ ${prefix}drakor [query]
+├ 👻💫✇ ${prefix}anime [query]
+├ 👻💫✇ ${prefix}character [query]
+├ 👻💫✇ ${prefix}manga [query]
+└───────
+
+┌── 「 *Random Menu* 」
+├ 👻💫✇ ${prefix}coffe
+├ 👻💫✇ ${prefix}quotesanime
+├ 👻💫✇ ${prefix}motivasi
+├ 👻💫✇ ${prefix}dilanquote
+├ 👻💫✇ ${prefix}bucinquote
+├ 👻💫✇ ${prefix}katasenja
+├ 👻💫✇ ${prefix}puisi
+├ 👻💫✇ ${prefix}couple
+├ 👻💫✇ ${prefix}anime
+├ 👻💫✇ ${prefix}waifu
+├ 👻💫✇ ${prefix}husbu
+├ 👻💫✇ ${prefix}neko
+├ 👻💫✇ ${prefix}shinobu
+├ 👻💫✇ ${prefix}waifus (nsfw)
+├ 👻💫✇ ${prefix}nekos (nsfw)
+├ 👻💫✇ ${prefix}trap (nsfw)
+├ 👻💫✇ ${prefix}blowjob (nsfw)
+└───────
+
+┌── 「 *Random Anime Menu* 」
+├ 👻💫✇ ${prefix}loli
+├ 👻💫✇ ${prefix}neko
+├ 👻💫✇ ${prefix}waifu
+├ 👻💫✇ ${prefix}shinobu
+├ 👻💫✇ ${prefix}megumin
+├ 👻💫✇ ${prefix}bully
+├ 👻💫✇ ${prefix}cuddle
+├ 👻💫✇ ${prefix}cry
+├ 👻💫✇ ${prefix}hug
+├ 👻💫✇ ${prefix}awoo
+├ 👻💫✇ ${prefix}kiss
+├ 👻💫✇ ${prefix}lick
+├ 👻💫✇ ${prefix}pat
+├ 👻💫✇ ${prefix}smug
+├ 👻💫✇ ${prefix}bonk
+├ 👻💫✇ ${prefix}yeet
+├ 👻💫✇ ${prefix}blush
+├ 👻💫✇ ${prefix}smile
+├ 👻💫✇ ${prefix}wave
+├ 👻💫✇ ${prefix}highfive
+├ 👻💫✇ ${prefix}handhold
+├ 👻💫✇ ${prefix}nom
+├ 👻💫✇ ${prefix}bite
+├ 👻💫✇ ${prefix}glomp
+├ 👻💫✇ ${prefix}slap
+├ 👻💫✇ ${prefix}kill
+├ 👻💫✇ ${prefix}happy
+├ 👻💫✇ ${prefix}wink
+├ 👻💫✇ ${prefix}poke
+├ 👻💫✇ ${prefix}dance
+├ 👻💫✇ ${prefix}cringe
+└───────
+
+┌── 「 *Text Pro Menu* 」
+├ 👻💫✇ ${prefix}3dchristmas
+├ 👻💫✇ ${prefix}3ddeepsea
+├ 👻💫✇ ${prefix}americanflag
+├ 👻💫✇ ${prefix}3dscifi
+├ 👻💫✇ ${prefix}3drainbow
+├ 👻💫✇ ${prefix}3dwaterpipe
+├ 👻💫✇ ${prefix}halloweenskeleton
+├ 👻💫✇ ${prefix}sketch
+├ 👻💫✇ ${prefix}bluecircuit
+├ 👻💫✇ ${prefix}space
+├ 👻💫✇ ${prefix}metallic
+├ 👻💫✇ ${prefix}fiction
+├ 👻💫✇ ${prefix}greenhorror
+├ 👻💫✇ ${prefix}transformer
+├ 👻💫✇ ${prefix}berry
+├ 👻💫✇ ${prefix}thunder
+├ 👻💫✇ ${prefix}magma
+├ 👻💫✇ ${prefix}3dcrackedstone
+├ 👻💫✇ ${prefix}3dneonlight
+├ 👻💫✇ ${prefix}impressiveglitch
+├ 👻💫✇ ${prefix}naturalleaves
+├ 👻💫✇ ${prefix}fireworksparkle
+├ 👻💫✇ ${prefix}matrix
+├ 👻💫✇ ${prefix}dropwater
+├ 👻💫✇ ${prefix}harrypotter
+├ 👻💫✇ ${prefix}foggywindow
+├ 👻💫✇ ${prefix}neondevils
+├ 👻💫✇ ${prefix}christmasholiday
+├ 👻💫✇ ${prefix}3dgradient
+├ 👻💫✇ ${prefix}blackpink
+├ 👻💫✇ ${prefix}gluetext
+└───────
+
+┌── 「 *Photo Oxy Menu* 」
+├ 👻💫✇ ${prefix}shadow
+├ 👻💫✇ ${prefix}romantic
+├ 👻💫✇ ${prefix}smoke
+├ 👻💫✇ ${prefix}burnpapper
+├ 👻💫✇ ${prefix}naruto
+├ 👻💫✇ ${prefix}lovemsg
+├ 👻💫✇ ${prefix}grassmsg
+├ 👻💫✇ ${prefix}lovetext
+├ 👻💫✇ ${prefix}coffecup
+├ 👻💫✇ ${prefix}butterfly
+├ 👻💫✇ ${prefix}harrypotter
+├ 👻💫✇ ${prefix}retrolol
+└───────
+
+┌── 「 *Ephoto Menu* 」
+├ 👻💫✇ ${prefix}ffcover
+├ 👻💫✇ ${prefix}crossfire
+├ 👻💫✇ ${prefix}galaxy
+├ 👻💫✇ ${prefix}glass
+├ 👻💫✇ ${prefix}neon
+├ 👻💫✇ ${prefix}beach
+├ 👻💫✇ ${prefix}blackpink
+├ 👻💫✇ ${prefix}igcertificate
+├ 👻💫✇ ${prefix}ytcertificate
+└───────
+
+┌── 「 *Fun Menu* 」
+├ 👻💫✇ ${prefix}bagaimanakah
+├ 👻💫✇ ${prefix}kapankah
+├ 👻💫✇ ${prefix}apakah
+├ 👻💫✇ ${prefix}bisakah
+├ 👻💫✇ ${prefix}rate
+├ 👻💫✇ ${prefix}wangy
+├ 👻💫✇ ${prefix}gantengcek
+├ 👻💫✇ ${prefix}cekganteng
+├ 👻💫✇ ${prefix}cantikcek
+├ 👻💫✇ ${prefix}cekcantik
+├ 👻💫✇ ${prefix}sangecek
+├ 👻💫✇ ${prefix}ceksange
+├ 👻💫✇ ${prefix}cekme
+├ 👻💫✇ ${prefix}gaycek
+├ 👻💫✇ ${prefix}cekgay
+├ 👻💫✇ ${prefix}lesbicek
+├ 👻💫✇ ${prefix}halah
+├ 👻💫✇ ${prefix}hilih
+├ 👻💫✇ ${prefix}huluh
+├ 👻💫✇ ${prefix}heleh
+├ 👻💫✇ ${prefix}holoh
+├ 👻💫✇ ${prefix}jadian
+├ 👻💫✇ ${prefix}jodohku
+├ 👻💫✇ ${prefix}delttt
+├ 👻💫✇ ${prefix}tictactoe
+├ 👻💫✇ ${prefix}family100
+├ 👻💫✇ ${prefix}tebak [option]
+├ 👻💫✇ ${prefix}math [mode]
+├ 👻💫✇ ${prefix}suitpvp [@tag]
+└───────
+
+┌── 「 *Convert Menu* 」
+├ 👻💫✇ ${prefix}toimage
+├ 👻💫✇ ${prefix}removebg
+├ 👻💫✇ ${prefix}sticker
+├ 👻💫✇ ${prefix}emojimix
+├ 👻💫✇ ${prefix}tovideo
+├ 👻💫✇ ${prefix}togif
+├ 👻💫✇ ${prefix}tourl
+├ 👻💫✇ ${prefix}tovn
+├ 👻💫✇ ${prefix}tomp3
+├ 👻💫✇ ${prefix}toaudio
+├ 👻💫✇ ${prefix}ebinary
+├ 👻💫✇ ${prefix}dbinary
+├ 👻💫✇ ${prefix}styletext
+├ 👻💫✇ ${prefix}smeme
+└───────
+
+┌── 「 *Main Menu* 」
+├ 👻💫✇ ${prefix}script
+├ 👻💫✇ ${prefix}speedtest
+├ 👻💫✇ ${prefix}ping
+├ 👻💫✇ ${prefix}owner
+├ 👻💫✇ ${prefix}menu
+├ 👻💫✇ ${prefix}delete
+├ 👻💫✇ ${prefix}infochat
+├ 👻💫✇ ${prefix}quoted
+├ 👻💫✇ ${prefix}listpc
+├ 👻💫✇ ${prefix}listgc
+├ 👻💫✇ ${prefix}listonline
+└───────
+
+┌── 「 *Database Menu* 」
+├ 👻💫✇ ${prefix}setcmd
+├ 👻💫✇ ${prefix}listcmd
+├ 👻💫✇ ${prefix}delcmd
+├ 👻💫✇ ${prefix}lockcmd
+├ 👻💫✇ ${prefix}addmsg
+├ 👻💫✇ ${prefix}listmsg
+├ 👻💫✇ ${prefix}getmsg
+├ 👻💫✇ ${prefix}delmsg
+└───────
+
+┌── 「 *Anonymous Menu* 」
+├ 👻💫✇ ${prefix}anonymous
+├ 👻💫✇ ${prefix}start
+├ 👻💫✇ ${prefix}next
+├ 👻💫✇ ${prefix}keluar
+├ 👻💫✇ ${prefix}sendkontak
+└───────
+
+
+
+┌── 「 *Voice Changer* 」
+├ 👻💫✇ ${prefix}bass
+├ 👻💫✇ ${prefix}blown
+├ 👻💫✇ ${prefix}deep
+├ 👻💫✇ ${prefix}earrape
+├ 👻💫✇ ${prefix}fast
+├ 👻💫✇ ${prefix}fat
+├ 👻💫✇ ${prefix}nightcore
+├ 👻💫✇ ${prefix}reverse
+├ 👻💫✇ ${prefix}robot
+├ 👻💫✇ ${prefix}slow
+├ 👻💫✇ ${prefix}tupai
+└───────
+
+┌── 「 *Owner Menu* 」
+├ 👻💫✇ ${prefix}react [emoji]
+├ 👻💫✇ ${prefix}chat [option]
+├ 👻💫✇ ${prefix}join [link]
+├ 👻💫✇ ${prefix}leave
+├ 👻💫✇ ${prefix}block @user
+├ 👻💫✇ ${prefix}unblock @user
+├ 👻💫✇ ${prefix}bcgroup [text]
+├ 👻💫✇ ${prefix}bcall [text]
+├ 👻💫✇ ${prefix}setppbot [image]
+├ 👻💫✇ ${prefix}setexif
+├ 👻💫✇ ${prefix}setmenu
+└───────
+
+┌── 「 *Thanks To* 」
+├ 👻💫✇ VIMUKTHI OSHADA 
+└───────
+`
+}
+
+// Donasi
+exports.donasi  = (ownernomer) => {
+	return`
+-------「 *DONATE* 」-------
+
+Hai kak ☺️ 
+Kalian bisa mendukung saya agar bot ini tetap up to date dengan cara donasi
+Berapapun donasi kalian akan sangat berarti 👍
+Thanks!
+
+-------「 *SEWA | JADI BOT | RUN BOT* 」-------
+
+*_SEWA BOT_* 
+- _Sewa Bot 15k (1 minggu)_
+- _Sewa Bot 20k (1 bulan)_
+- _Sewa Bot 25k (Permanen)_
+
+*_JADI BOT_* 
+- _Jadi Bot 15k Tidak Owner (1 minggu)_
+- _Jadi Bot + Owner 20k (1 bulan)_
+- _Jadi Bot + Owner 25k (Permanen)_
+
+*_JASA RUN BOT_*
+- _Run Bot 15k (1 minggu)_
+- _Run Bot 20k (1 bulan)_
+- _Run Bot 25k (Permanen)_
+ 
+*_Minat? Langsung chat aja ya cuy 🐦_*
+
+*_Contact person Owner_* :
+wa.me/${ownernomer}
+`
+}
+
+// Simple Menu
+exports.groupmenu = (prefix) => {
+	return`
+┌── 「 *Group Menu* 」
+├ 👻💫✇ ${prefix}linkgroup
+├ 👻💫✇ ${prefix}ephemeral [option]
+├ 👻💫✇ ${prefix}setppgc [image]
+├ 👻💫✇ ${prefix}setname [text]
+├ 👻💫✇ ${prefix}setdesc [text]
+├ 👻💫✇ ${prefix}group [option]
+├ 👻💫✇ ${prefix}editinfo [option]
+├ 👻💫✇ ${prefix}add @user
+├ 👻💫✇ ${prefix}kick @user
+├ 👻💫✇ ${prefix}hidetag [text]
+├ 👻💫✇ ${prefix}tagall [text]
+├ 👻💫✇ ${prefix}antilink [on/off]
+├ 👻💫✇ ${prefix}mute [on/off]
+├ 👻💫✇ ${prefix}promote @user
+├ 👻💫✇ ${prefix}demote @user
+├ 👻💫✇ ${prefix}vote [text]
+├ 👻💫✇ ${prefix}devote
+├ 👻💫✇ ${prefix}upvote
+├ 👻💫✇ ${prefix}cekvote
+├ 👻💫✇ ${prefix}hapusvote
+└───────
+`
+}
+
+
+`
+}
+
+exports.downloadermenu = (prefix) => {
+	return`
+┌── 「 *Downloader Menu* 」
+├ 👻💫✇ ${prefix}tiktoknowm [url]
+├ 👻💫✇ ${prefix}tiktokwm [url]
+├ 👻💫✇ ${prefix}tiktokmp3 [url]
+├ 👻💫✇ ${prefix}instagram [url]
+├ 👻💫✇ ${prefix}twitter [url]
+├ 👻💫✇ ${prefix}twittermp3 [url]
+├ 👻💫✇ ${prefix}facebook [url]
+├ 👻💫✇ ${prefix}pinterestdl [url]
+├ 👻💫✇ ${prefix}ytmp3 [url]
+├ 👻💫✇ ${prefix}ytmp4 [url]
+├ 👻💫✇ ${prefix}getmusic [query]
+├ 👻💫✇ ${prefix}getvideo [query]
+├ 👻💫✇ ${prefix}umma [url]
+├ 👻💫✇ ${prefix}joox [query]
+├ 👻💫✇ ${prefix}soundcloud [url]
+└───────
+`
+}
+
+exports.searchmenu = (prefix) => {
+	return`
+┌── 「 *Search Menu* 」
+├ 👻💫✇ ${prefix}play [query]
+├ 👻💫✇ ${prefix}yts [query]
+├ 👻💫✇ ${prefix}google [query]
+├ 👻💫✇ ${prefix}gimage [query]
+├ 👻💫✇ ${prefix}pinterest [query]
+├ 👻💫✇ ${prefix}wallpaper [query]
+├ 👻💫✇ ${prefix}wikimedia [query]
+├ 👻💫✇ ${prefix}ytsearch [query]
+├ 👻💫✇ ${prefix}ringtone [query]
+├ 👻💫✇ ${prefix}stalk [option] [query]
+├ 👻💫✇ ${prefix}webtoons [query]
+├ 👻💫✇ ${prefix}drakor [query]
+├ 👻💫✇ ${prefix}anime [query]
+├ 👻💫✇ ${prefix}character [query]
+├ 👻💫✇ ${prefix}manga [query]
+└───────
+`
+}
+
+exports.randommenu = (prefix) => {
+	return`
+┌── 「 *Random Menu* 」
+├ 👻💫✇ ${prefix}coffe
+├ 👻💫✇ ${prefix}quotesanime
+├ 👻💫✇ ${prefix}motivasi
+├ 👻💫✇ ${prefix}dilanquote
+├ 👻💫✇ ${prefix}bucinquote
+├ 👻💫✇ ${prefix}katasenja
+├ 👻💫✇ ${prefix}puisi
+├ 👻💫✇ ${prefix}couple
+├ 👻💫✇ ${prefix}anime
+├ 👻💫✇ ${prefix}waifu
+├ 👻💫✇ ${prefix}husbu
+├ 👻💫✇ ${prefix}neko
+├ 👻💫✇ ${prefix}shinobu
+├ 👻💫✇ ${prefix}waifus (nsfw)
+├ 👻💫✇ ${prefix}nekos (nsfw)
+├ 👻💫✇ ${prefix}trap (nsfw)
+├ 👻💫✇ ${prefix}blowjob (nsfw)
+└───────
+`
+}
+
+exports.randomanimemenu = (prefix) => {
+	return`
+┌── 「 *Random Anime Menu* 」
+├ 👻💫✇ ${prefix}loli
+├ 👻💫✇ ${prefix}neko
+├ 👻💫✇ ${prefix}waifu
+├ 👻💫✇ ${prefix}shinobu
+├ 👻💫✇ ${prefix}megumin
+├ 👻💫✇ ${prefix}bully
+├ 👻💫✇ ${prefix}cuddle
+├ 👻💫✇ ${prefix}cry
+├ 👻💫✇ ${prefix}hug
+├ 👻💫✇ ${prefix}awoo
+├ 👻💫✇ ${prefix}kiss
+├ 👻💫✇ ${prefix}lick
+├ 👻💫✇ ${prefix}pat
+├ 👻💫✇ ${prefix}smug
+├ 👻💫✇ ${prefix}bonk
+├ 👻💫✇ ${prefix}yeet
+├ 👻💫✇ ${prefix}blush
+├ 👻💫✇ ${prefix}smile
+├ 👻💫✇ ${prefix}wave
+├ 👻💫✇ ${prefix}highfive
+├ 👻💫✇ ${prefix}handhold
+├ 👻💫✇ ${prefix}nom
+├ 👻💫✇ ${prefix}bite
+├ 👻💫✇ ${prefix}glomp
+├ 👻💫✇ ${prefix}slap
+├ 👻💫✇ ${prefix}kill
+├ 👻💫✇ ${prefix}happy
+├ 👻💫✇ ${prefix}wink
+├ 👻💫✇ ${prefix}poke
+├ 👻💫✇ ${prefix}dance
+├ 👻💫✇ ${prefix}cringe
+└───────
+`
+}
+
+exports.textpromenu = (prefix) => {
+	return`
+┌── 「 *Text Pro Menu* 」
+├ 👻💫✇ ${prefix}3dchristmas
+├ 👻💫✇ ${prefix}3ddeepsea
+├ 👻💫✇ ${prefix}americanflag
+├ 👻💫✇ ${prefix}3dscifi
+├ 👻💫✇ ${prefix}3drainbow
+├ 👻💫✇ ${prefix}3dwaterpipe
+├ 👻💫✇ ${prefix}halloweenskeleton
+├ 👻💫✇ ${prefix}sketch
+├ 👻💫✇ ${prefix}bluecircuit
+├ 👻💫✇ ${prefix}space
+├ 👻💫✇ ${prefix}metallic
+├ 👻💫✇ ${prefix}fiction
+├ 👻💫✇ ${prefix}greenhorror
+├ 👻💫✇ ${prefix}transformer
+├ 👻💫✇ ${prefix}berry
+├ 👻💫✇ ${prefix}thunder
+├ 👻💫✇ ${prefix}magma
+├ 👻💫✇ ${prefix}3dcrackedstone
+├ 👻💫✇ ${prefix}3dneonlight
+├ 👻💫✇ ${prefix}impressiveglitch
+├ 👻💫✇ ${prefix}naturalleaves
+├ 👻💫✇ ${prefix}fireworksparkle
+├ 👻💫✇ ${prefix}matrix
+├ 👻💫✇ ${prefix}dropwater
+├ 👻💫✇ ${prefix}harrypotter
+├ 👻💫✇ ${prefix}foggywindow
+├ 👻💫✇ ${prefix}neondevils
+├ 👻💫✇ ${prefix}christmasholiday
+├ 👻💫✇ ${prefix}3dgradient
+├ 👻💫✇ ${prefix}blackpink
+├ 👻💫✇ ${prefix}gluetext
+└───────
+`
+}
+
+exports.photooxymenu = (prefix) => {
+	return`
+┌── 「 *Photo Oxy Menu* 」
+├ 👻💫✇ ${prefix}shadow
+├ 👻💫✇ ${prefix}romantic
+├ 👻💫✇ ${prefix}smoke
+├ 👻💫✇ ${prefix}burnpapper
+├ 👻💫✇ ${prefix}naruto
+├ 👻💫✇ ${prefix}lovemsg
+├ 👻💫✇ ${prefix}grassmsg
+├ 👻💫✇ ${prefix}lovetext
+├ 👻💫✇ ${prefix}coffecup
+├ 👻💫✇ ${prefix}butterfly
+├ 👻💫✇ ${prefix}harrypotter
+├ 👻💫✇ ${prefix}retrolol
+└───────
+`
+}
+
+exports.ephotomenu = (prefix) => {
+	return`
+┌── 「 *Ephoto Menu* 」
+├ 👻💫✇ ${prefix}ffcover
+├ 👻💫✇ ${prefix}crossfire
+├ 👻💫✇ ${prefix}galaxy
+├ 👻💫✇ ${prefix}glass
+├ 👻💫✇ ${prefix}neon
+├ 👻💫✇ ${prefix}beach
+├ 👻💫✇ ${prefix}blackpink
+├ 👻💫✇ ${prefix}igcertificate
+├ 👻💫✇ ${prefix}ytcertificate
+└───────
+`
+}
+
+exports.funmenu = (prefix) => {
+	return`
+┌── 「 *Fun Menu* 」
+├ 👻💫✇ ${prefix}bagaimanakah
+├ 👻💫✇ ${prefix}kapankah
+├ 👻💫✇ ${prefix}apakah
+├ 👻💫✇ ${prefix}bisakah
+├ 👻💫✇ ${prefix}rate
+├ 👻💫✇ ${prefix}wangy
+├ 👻💫✇ ${prefix}gantengcek
+├ 👻💫✇ ${prefix}cekganteng
+├ 👻💫✇ ${prefix}cantikcek
+├ 👻💫✇ ${prefix}cekcantik
+├ 👻💫✇ ${prefix}sangecek
+├ 👻💫✇ ${prefix}ceksange
+├ 👻💫✇ ${prefix}cekme
+├ 👻💫✇ ${prefix}gaycek
+├ 👻💫✇ ${prefix}cekgay
+├ 👻💫✇ ${prefix}lesbicek
+├ 👻💫✇ ${prefix}halah
+├ 👻💫✇ ${prefix}hilih
+├ 👻💫✇ ${prefix}huluh
+├ 👻💫✇ ${prefix}heleh
+├ 👻💫✇ ${prefix}holoh
+├ 👻💫✇ ${prefix}jadian
+├ 👻💫✇ ${prefix}jodohku
+├ 👻💫✇ ${prefix}delttt
+├ 👻💫✇ ${prefix}tictactoe
+├ 👻💫✇ ${prefix}family100
+├ 👻💫✇ ${prefix}tebak [option]
+├ 👻💫✇ ${prefix}math [mode]
+├ 👻💫✇ ${prefix}suitpvp [@tag]
+└───────
+`
+}
+
+exports.primbonmenu = (prefix) => {
+	return`
+
+`
+}
+
+exports.convertmenu = (prefix) => {
+	return`
+┌── 「 *Convert Menu* 」
+├ 👻💫✇ ${prefix}toimage
+├ 👻💫✇ ${prefix}removebg
+├ 👻💫✇ ${prefix}sticker
+├ 👻💫✇ ${prefix}emojimix
+├ 👻💫✇ ${prefix}tovideo
+├ 👻💫✇ ${prefix}togif
+├ 👻💫✇ ${prefix}tourl
+├ 👻💫✇ ${prefix}tovn
+├ 👻💫✇ ${prefix}tomp3
+├ 👻💫✇ ${prefix}toaudio
+├ 👻💫✇ ${prefix}ebinary
+├ 👻💫✇ ${prefix}dbinary
+├ 👻💫✇ ${prefix}styletext
+└───────
+`
+}
+
+exports.mainmenu = (prefix) => {
+	return`
+┌── 「 *Main Menu* 」
+├ 👻💫✇ ${prefix}script
+├ 👻💫✇ ${prefix}speedtest
+├ 👻💫✇ ${prefix}ping
+├ 👻💫✇ ${prefix}owner
+├ 👻💫✇ ${prefix}menu / ${prefix}help / ${prefix}?
+├ 👻💫✇ ${prefix}delete
+├ 👻💫✇ ${prefix}infochat
+├ 👻💫✇ ${prefix}quoted
+├ 👻💫✇ ${prefix}listpc
+├ 👻💫✇ ${prefix}listgc
+├ 👻💫✇ ${prefix}listonline
+└───────
+`
+}
+
+exports.databasemenu = (prefix) => {
+	return`
+┌── 「 *Database Menu* 」
+├ 👻💫✇ ${prefix}setcmd
+├ 👻💫✇ ${prefix}listcmd
+├ 👻💫✇ ${prefix}delcmd
+├ 👻💫✇ ${prefix}lockcmd
+├ 👻💫✇ ${prefix}addmsg
+├ 👻💫✇ ${prefix}listmsg
+├ 👻💫✇ ${prefix}getmsg
+├ 👻💫✇ ${prefix}delmsg
+`
+}
+
+exports.anonymousmenu = (prefix) => {
+	return`
+┌── 「 *Anonymous Menu* 」
+├ 👻💫✇ ${prefix}anonymous
+├ 👻💫✇ ${prefix}start
+├ 👻💫✇ ${prefix}next
+├ 👻💫✇ ${prefix}keluar
+├ 👻💫✇ ${prefix}sendkontak
+└───────
+`
+}
+
+exports.islamicmenu = (prefix) => {
+	return`
+
+`
+}
+
+exports.voicechargermenu = (prefix) => {
+	return`
+┌── 「 *Voice Changer「 *
+├ 👻💫✇ ${prefix}bass
+├ 👻💫✇ ${prefix}blown
+├ 👻💫✇ ${prefix}deep
+├ 👻💫✇ ${prefix}earrape
+├ 👻💫✇ ${prefix}fast
+├ 👻💫✇ ${prefix}fat
+├ 👻💫✇ ${prefix}nightcore
+├ 👻💫✇ ${prefix}reverse
+├ 👻💫✇ ${prefix}robot
+├ 👻💫✇ ${prefix}slow
+├ 👻💫✇ ${prefix}tupai
+└───────
+`
+}
+
+exports.ownermenu = (prefix) => {
+	return`
+┌── 「 *Owner Menu* 」
+├ 👻💫✇ ${prefix}react [emoji]
+├ 👻💫✇ ${prefix}chat [option]
+├ 👻💫✇ ${prefix}join [link]
+├ 👻💫✇ ${prefix}leave
+├ 👻💫✇ ${prefix}block @user
+├ 👻💫✇ ${prefix}unblock @user
+├ 👻💫✇ ${prefix}bcgroup [text]
+├ 👻💫✇ ${prefix}bcall [text]
+├ 👻💫✇ ${prefix}setppbot [image]
+├ 👻💫✇ ${prefix}setexif
+├ 👻💫✇ ${prefix}setmenu
+└───────
+`
+}
+
+exports.thanksto = () => {
+	return`
+┌── 「 *Thanks To* 」
+├ 👻💫✇ VIMUKTHI OSHADA
+└───────
+`
+}
---
+++ settings.js
@@ -0,0 +1,74 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Apikey kalian', // Masukin apikey kalian
+}
+
+// Other
+global.owner = ['94775792013']
+global.premium = ['94775792013']
+global.ownernomer = '94775792013'
+global.ownername = 'ZERO BoT HUB'
+global.botname = 'ZERO-MD'
+global.footer = 'ZERO-MD'
+global.ig = 'https://instagram.com/vimukthi.oshada'
+global.email = 'oshadavimukthi76@gmail.com'
+global.region = 'sri lanka :)'
+global.sc = 'https://youtube.com/channel/UCzEdH_d_z82l9AI5JDghc0w'
+global.myweb = 'https://youtube.com/channel/UCzEdH_d_z82l9AI5JDghc0w'
+global.packname = 'ZERO-Md'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'SESSION'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Fitur sedang error!',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./image/chika.jpg')
+global.asek = fs.readFileSync('./image/velo.mp4')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
